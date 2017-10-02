dda6ef735c1ef24304290a32f233dafdd84eda82
Gerard Lopez <glopes@pasiona.com>
2017-10-02 13:11:10 +0200

    Revert "Add examples in TS"
    This reverts commit 6d9ea71570acad6acaf02ad82ca4038b9e729ef4.

--- examplesTS/00 intro/01 Import/averageService.ts
+++
@@ -1,9 +0,0 @@
-export function getAvg(scores:number[]):number {
- return getTotalScore(scores) / scores.length;
-}
-
-function getTotalScore(scores:number[]):number {
-  return scores.reduce((totalScore, currentScore) => {
-    return totalScore + currentScore;
-  });
-}
